0e17c8fcda4f47971f9d305b8d91be2bc46e9c42
eduardo-braga <eduardocomp@gmail.com>
2025-05-17 22:52:09 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  safelist: [
+    {
+      pattern: /data-\[state=active\].*/,
+      variants: ["responsive", "hover", "focus", "active"],
+    }
+  ]
+};
