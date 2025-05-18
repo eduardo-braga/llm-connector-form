64348c63e986a78c98601b60dc525088023f4d38
eduardo-braga <eduardocomp@gmail.com>
2025-05-17 21:14:22 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AiApiCallForm from './pages/LLM_Connector_Form';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AiApiCallForm />
+  </React.StrictMode>
+);
