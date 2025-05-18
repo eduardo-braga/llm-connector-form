935f17f258398d73b136da5d665fa96940105a5d
eduardo-braga <eduardocomp@gmail.com>
2025-05-17 22:26:33 -0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AiApiCallForm from "./pages/LLM_Connector_Form";
-
-function App() {
-  return <AiApiCallForm />;
-}
-
-export default App;
--- src/components/ui/textarea.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export function Textarea({ className = "", ...props }) {
-  return <textarea className={`flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm ${className}`} {...props} />;
-}
